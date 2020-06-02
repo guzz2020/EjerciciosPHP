cadf6cd123dd7e31c8fc3673c0c773ba3b741e3a
guzz2020 <51685211+guzz2020@users.noreply.github.com>
2020-06-02 13:01:07 -0600

    Update E-5

--- Ejercicio-5.php
+++ Ejercicio-5.php
@@ -4,16 +4,16 @@
 
 <h1>Ejercicio 5</h1>
 <?php
-
 // Este ejercicio corresponde a utilizacion de Ciclos en PHP
-
-header("Content-type: text/html; charset= \"utf-8\"");
+header("Content-type: text/html;charset=\"utf-8\"");
 
 for ($i=1; $i<=10; $i++)
 {
-    echo "<p> Finaliza el ciclo for</p>"; 
+    echo "<p>$i</p>"; 
 }
-for ($i=10; $i<=0; $i--)
+echo "<p> Finaliza el ciclo for</p>"; 
+
+for ($i=10; $i>=0; $i--)
 {
     echo "<p>$i</p>"; 
 }
@@ -21,25 +21,18 @@ echo "<p>Finaliza el ciclo for</p>";
 
 $familia = array("Maria", "Ana", "Jose", "Pedro");
 
-for($i=0;$i<= sizeof($familia); i++)
+for($i=0;$i<=sizeof($familia); i++)
 {
-    echo"<h2>$familia[$i] nuero de elemento $i </h2>";
+    echo"<h2>$familia[$i] numero de elemento $i </h2>";
     echo "<br>";
 }
   
-
 //Agregando el ciclo while
 $n =0; //el contador de posicion 
  while ($n < count($familia)){
      $++; //aumenta en uno cada vez que pasa el ciclo
      echo $familia[$n]. "br />";
  }
-
-
-
-
-
-
 
 ?>
 </body>
