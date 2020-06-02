8e4db45f17a5297972db9a473eedc119f05be4b9
guzz2020 <51685211+guzz2020@users.noreply.github.com>
2020-06-02 12:55:04 -0600

    Update Ejercicio-3.php

--- Ejercicio-3.php
+++ Ejercicio-3.php
@@ -3,23 +3,26 @@
 <body>
 
 <h1>Ejercicio 3</h1>
+
 <?php
-    echo "<h2> Arreglos </h2>";
+//Arreglos
+    echo "<h2>Arreglos</h2>";
     $miarreglo = array (10,20,30,40,50);
 
     echo "<p>  Contenido del elemento 2 del arreglo: $miarreglo[2]</p>";
     echo "<p> Despliega el contenido del arreglo: </p>";
     print_r ($miarreglo);// Se utiliza esta funcion para ver de forma entendible el arreglo
-    echo " <p> El tamaño del arreglo es:" .sizeof($miarreglo)  "</p>";
+ 
+    echo " <p> El tamaño del arreglo es:" .sizeof($miarreglo).  "</p>";
 
     $miFechaArr["dia"]= "Lunes";
     $miFechaArr["mes"]= "Junio";
     $miFechaArr["dia"]= "2020";
 
     echo"<br><br>";
-    <var_dump ( $miFechaArr); //Muestra Info de la variable del arreglo
+    var_dump ($miFechaArr); //Muestra Info de la variable del arreglo
 
-    unset ($miFechaArr["año"]);
+    unset ($miFechaArr["año"]); //Elimina el Elemento Indicado del arreglo
     echo"<br><br>";
     print_r ($miFechaArr);
     $miarregloReves = array_reverse($miarreglo);
